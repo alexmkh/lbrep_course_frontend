673272c21b1cf8419add87162f832777d50e0745
alexmkh <alexmkh33@gmail.com>
2025-10-30 16:53:21 +0000

    Changed URL in Listings.jsx

--- src/Components/Listings.jsx
+++ src/Components/Listings.jsx
@@ -96,7 +96,7 @@ function Listings() {
     const GetAllListings = async () => {
       try {
         const response = await Axios.get(
-          "http://localhost:8000/api/listings/",
+          "http://lbrep.alexmkh.com.es/api/listings/",
           { cancelToken: source.token }
         );
         // console.log(response.data);
